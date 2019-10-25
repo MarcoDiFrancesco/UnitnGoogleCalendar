cae87561e7e5999504ce6c9b501d90f95333918e
Marco Di Francesco <marcodifran@gmail.com>
2019-10-25 10:44:42 +0200

    Update fetch calendars with async functions

--- index.js
+++ index.js
@@ -9,39 +9,49 @@ const underscore = require('underscore');
 const port = 3000;
 const app = express();
 
-downloadRequest();
-setInterval(downloadNewCal, 3000);
+const downloadNewCal = (async (url, path) => {
+  const res = await fetch(url);
+  let fileName = "cal-" + Date.now() + ".ics";
+  const file = fs.createWriteStream(path + fileName);
+  await new Promise((resolve, reject) => {
+    res.body.pipe(file); // Fetch into file
+    res.body.on("error", (err) => {
+      reject(err);
+    });
+    res.body.on("finish", function () {
+      resolve();
+    })
+  })
+  console.log('Download from Unitn server ' + fileName);
+});
 
-function downloadNewCal(req, res) {
-  const url = 'https://easyacademy.unitn.it/AgendaStudentiUnitn/ec_download_ical_list.php?form-type=corso&anno=2019&corso=0514G&anno2%5B%5D=P0005%7C2';
-  var fileName = "cal-" + Date.now() + ".ics"; // Name of the downloaded file
-  var pathName = 'temp/'; // Folder where the file will be stored
-  const fileDown = fs.createWriteStream(pathName + fileName); // Complete path of the directory + file name
-  const request = https.get(url, function(response) { // Request file from url
-    response.pipe(fileDown); // Write content into this file
-    console.log('Download from Unitn server ' + fileName);
-  });
-}
+setInterval(
+  function() {
+    downloadNewCal('https://easyacademy.unitn.it/AgendaStudentiUnitn/ec_download_ical_list.php?form-type=corso&anno=2019&corso=0514G&anno2%5B%5D=P0005%7C2', 'temp/')
+  }
+, 1440000);
 
 // Get latest downloaded file
-function downloadRequest() {
+const downloadRequest = () => {
   app.get('/download/', (req, res) => {
-    var dir = './temp';
-    var fileName = getLatestFile(dir);
+    let dir = './temp';
+    let fileName = getLatestFile(dir);
     console.log(fileName + ' downloaded from Unitn serves');
-    var fileDir = path.join(dir, fileName);
+    let fileDir = path.join(dir, fileName);
     res.download(fileDir, fileName);
   });
 }
+
+downloadRequest();
 
 app.listen(port, () => {
   console.log('Server running http://127.0.0.1:3000');
 });
 
-function getLatestFile(dir) {
-  var files = fs.readdirSync(dir);
-  return underscore.max(files, function(fileName_) {
-    var fullPath = path.join(dir, fileName_);
+const getLatestFile = (dir) => {
+  let files = fs.readdirSync(dir);
+  return underscore.max(files, function (fileName_) {
+    let fullPath = path.join(dir, fileName_);
     return fs.statSync(fullPath).ctime;
   });
 }
